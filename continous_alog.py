b5cdc645e820d19fae7f47224aacde00d3f58f84
xuxin <xuxin-owo@sjtu.edu.cn>
2023-06-09 20:14:01 +0800

    增加信息素更新方法，添加地图绘制器

--- continous_alog.py
+++ continous_alog.py
@@ -2,44 +2,63 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
+import cv2
 
 class ant:
     def __init__(self,
                  start_x=150,
                  start_y=150,
-                 start_angle=0):
-        self.pos_x=start_x
-        self.pos_y=start_y
+                 start_angle=0,
+                 target_x=250,
+                 target_y=250,
+                 map_size=[300,300],
+                 obstacle_map=None,
+                 ):
+        self.current_pos_x=start_x
+        self.current_pos_y=start_y
         self.facing_angle=start_angle
+        self.arrive_ending=False
+        self.path=np.array([[start_x,start_y]])
+        self.target=[target_x,target_y]
+        self.map_size_x=map_size[0]
+        self.map_size_y=map_size[1]
+        if obstacle_map is None:
+            self.obstacle_map=np.zeros(map_size)
+        else:
+            self.obstacle_map=obstacle_map
     
-    def walk(self,
-             InfoDensityMap):
+    def judgeDirection(self,
+                       InfoDensityMap):
         #从前方区域中找信息素浓度较高的地方前进
-        x=self.pos_x
-        y=self.pos_y
+        x=self.current_pos_x
+        y=self.current_pos_y
         current_angle=self.facing_angle
         map_size_x=InfoDensityMap.shape[0]
         map_size_y=InfoDensityMap.shape[1]
-        sense_range=10
+
+        sense_radius=15#信息素搜集半径
+        #备选方向
+        left_angle=30
+        right_angle=-30
+        random_angle=30#角度随机范围:+r~-r
 
         left_info_density=0
         mid_info_density=0
         right_info_density=0
-
-        for ix in range(math.ceil(x-sense_range),math.floor(x+sense_range)):
-            for iy in range(math.ceil(y-sense_range),math.floor(y+sense_range)):
+        for ix in range(math.ceil(x-sense_radius),math.floor(x+sense_radius)):
+            for iy in range(math.ceil(y-sense_radius),math.floor(y+sense_radius)):
                 #排除超出地图边界的点
                 if (ix<0)or(iy<0)or(ix>=map_size_x)or(iy>=map_size_y):continue
                 #检测圆半径
                 r=math.sqrt((ix-x)**2+(iy-y)**2)
-                if r>sense_range:continue
+                if r>sense_radius:continue
                 #计算方位
                 info_angle=math.atan2(iy-y,ix-x)*180/math.pi
                 delta_angle=info_angle-current_angle
-                if(delta_angle>90)or(delta_angle<-90):continue
-                if(delta_angle>30):
+                if(delta_angle>left_angle/2*3)or(delta_angle<right_angle/2*3):continue
+                if(delta_angle>left_angle/2):
                     left_info_density+=InfoDensityMap[ix][iy]
-                elif(delta_angle>-30):
+                elif(delta_angle>right_angle/2):
                     mid_info_density+=InfoDensityMap[ix][iy]
                 else:
                     right_info_density+=InfoDensityMap[ix][iy]
@@ -52,49 +71,91 @@ class ant:
             angle_select=np.argmax([left_info_density,mid_info_density,right_info_density])            
         
         if(angle_select==0):
-            next_facing_angle=current_angle+60
+            next_facing_angle=current_angle+left_angle
         elif(angle_select==1):
             next_facing_angle=current_angle
         elif(angle_select==2):
-            next_facing_angle=current_angle-60
-        #随机一定角度
-        random_angle=5
+            next_facing_angle=current_angle+right_angle
+        
         next_facing_angle+=random.random()*random_angle*2-random_angle
         #整定到0~360度之间
         next_facing_angle=math.fmod(next_facing_angle,360)
-
-        #前进一步
-        step_length=3
-        next_pos_x=x+step_length*math.cos(next_facing_angle/180*math.pi)
-        next_pos_y=y+step_length*math.sin(next_facing_angle/180*math.pi)
-
-        #地图边界处理
-        obstacle_flag=False
-        if(next_pos_x<0)or(next_pos_x>map_size_x):
-            next_facing_angle=180-next_facing_angle
-            obstacle_flag=True
-        if(next_pos_y<0)or(next_pos_y>map_size_y):
-            next_facing_angle=-next_facing_angle
-            obstacle_flag=True
-
-        if(obstacle_flag):
-            #如果碰撞，还要更改一次方向
-            next_pos_x=x+step_length*math.cos(next_facing_angle/180*math.pi)
-            next_pos_y=y+step_length*math.sin(next_facing_angle/180*math.pi)
-
-        self.pos_x=next_pos_x
-        self.pos_y=next_pos_y
         self.facing_angle=next_facing_angle
 
+    def walk(self):
+        step_length=1
+
+        #前进一步
+        x=self.current_pos_x
+        y=self.current_pos_y
+        angle=self.facing_angle
+
+        next_pos_x=x+step_length*math.cos(angle/180*math.pi)
+        next_pos_y=y+step_length*math.sin(angle/180*math.pi)
+        self.facing_angle=angle
+        #地图边界处理，碰撞检测
+        if(self.obstacleDetection(x=next_pos_x,y=next_pos_y)):
+            #如果碰撞，还要更改一次方向
+            next_pos_x=x+step_length*math.cos(self.facing_angle/180*math.pi)
+            next_pos_y=y+step_length*math.sin(self.facing_angle/180*math.pi)
+
+        self.current_pos_x=next_pos_x
+        self.current_pos_y=next_pos_y
+
+        self.path=np.concatenate((self.path,[[next_pos_x,next_pos_y]]))
+
+    def isEnd(self):
+        x=self.current_pos_x
+        y=self.current_pos_y
+        tx=self.target[0]
+        ty=self.target[1]
+
+        threshold=25
+        dist=math.sqrt((x-tx)**2+(y-ty)**2)
+        if (dist<threshold):return True
+        else:return False
+
+    def obstacleDetection(self,
+                          x,
+                          y):
+        #地图边界处理
+        obstacle_flag=False
+        if(round(x)<0)or(round(x)>=map_size_x):
+            self.facing_angle=180-self.facing_angle
+            obstacle_flag=True
+        if(round(y)<0)or(round(y)>=map_size_y):
+            self.facing_angle=-self.facing_angle
+            obstacle_flag=True
+        if obstacle_flag:
+            self.facing_angle=math.fmod(self.facing_angle,360)
+            return True
+
+        #障碍检测
+        if self.obstacle_map[round(y)][round(x)]>0:
+            #图像和ndarray对应是先y坐标，再x坐标
+            self.facing_angle=self.facing_angle-180
+            self.facing_angle=math.fmod(self.facing_angle,360)
+            return True
+
+        return False
+
     def updateInfoDensity(self,
-                          InfoDensityMap):
-        x=self.pos_x
-        y=self.pos_y
+                          InfoDensityMap,
+                          x,
+                          y,
+                          update_intensity=1):
         #根据距离插值到临近的四个点上
         x0=math.floor(x)
         x1=math.ceil(x)
         y0=math.floor(y)
         y1=math.ceil(y)  
+        if(x0==x1):
+            #刚好整数格
+            x1=x0+1
+            x=x0+1e-5
+        if(y0==y1):
+            y1=y0+1
+            y=y0+1e-5 
         map_size_x=InfoDensityMap.shape[0]
         map_size_y=InfoDensityMap.shape[1]
         corner_points=[[x0,y0],[x0,y1],[x1,y1],[x1,y0]]
@@ -122,54 +183,100 @@ class ant:
             ix=point[0]
             iy=point[1]
             weight=corner_weight[i]
-            InfoDensityMap[ix][iy]+=weight*rhoTraverse
+            InfoDensityMap[ix][iy]+=weight*rhoTraverse*update_intensity
     
+    def updatePathInfoDensity(self,
+                              InfoDensityMap):
+        #信息素强度和路径长度有关
+        pathLength=0
+        for i in range(len(self.path)-1):
+            p1=self.path[i]
+            p2=self.path[i+1]
+            pathLength+=math.sqrt(np.sum((p1-p2)**2))
 
-ant_series=[ant()]
+        arrivalIntensity=1/pathLength*10000
+        for point in self.path:
+            self.updateInfoDensity(InfoDensityMap,
+                                   point[0],
+                                   point[1],
+                                   arrivalIntensity)
+
 accuracy=1.0
 map_size_x=300
 map_size_y=300
+map_size=[map_size_x,map_size_y]
 initial_infoDensity=0
 InfoDensityMap=np.ones((int(map_size_x/accuracy),
                         int(map_size_y/accuracy)))*initial_infoDensity
 
+# ObstacleMap=np.ones((int(map_size_x/accuracy),
+#                      int(map_size_y/accuracy)))
+ObstacleMap=cv2.imread('map1.png',cv2.IMREAD_GRAYSCALE)
+# cv2.imshow('123',ObstacleMap)
+ObstacleMap[ObstacleMap == 0] = 1
+ObstacleMap[ObstacleMap == 255] = 0
+ObstacleMap=np.flipud(ObstacleMap)#图像是上到下，坐标系是下到上，所以要颠倒一下
 fig,axes = plt.subplots(1,1)
 fig.set_size_inches([7,7]) # 设置图像大小
-
-
+# axes.imshow(ObstacleMap,cmap="binary",origin="upper")
+# plt.pause(0.01)
+ant_series=[ant(obstacle_map=ObstacleMap,map_size=map_size)]
 
 while True:
     #每秒增加一只蚂蚁
     # print(len(ant_series))
     if len(ant_series)<100:
-        ant_series.append(ant())
-        #TODO 初始化随机方向
+        angle=random.random()*360
+        ant_series.append(ant(start_angle=angle,
+                              obstacle_map=ObstacleMap,
+                              map_size=map_size))
+
     ant_x=[]
     ant_y=[]
     #对所有蚂蚁推进时间
-    for m_ant in ant_series:
-        m_ant.walk(InfoDensityMap=InfoDensityMap)
-        ant_x.append(m_ant.pos_x)
-        ant_y.append(m_ant.pos_y)
-    for m_ant in ant_series:
-        m_ant.updateInfoDensity(InfoDensityMap=InfoDensityMap)
+    for i in range(len(ant_series)):
+        m_ant=ant_series[i]
+        m_ant.judgeDirection(InfoDensityMap)
+        m_ant.walk()
+        ant_x.append(m_ant.current_pos_x)
+        ant_y.append(m_ant.current_pos_y)
+        if m_ant.isEnd():
+            m_ant.updatePathInfoDensity(InfoDensityMap)
+            angle=random.random()*360
+            ant_series[i]=ant(start_angle=angle,
+                              obstacle_map=ObstacleMap,
+                              map_size=map_size)
+    # for m_ant in ant_series:
+    #     m_ant.updateInfoDensity(InfoDensityMap,
+    #                             m_ant.current_pos_x,
+    #                             m_ant.current_pos_y)
+
     #自然蒸发率
-    rhoEvaporate=0.99
+    rhoEvaporate=0.9
     InfoDensityMap=np.multiply(InfoDensityMap,rhoEvaporate)
     
+    # 绘制蚂蚁
     axes.clear()
     axes.set_xlim(0, map_size_x)
     axes.set_ylim(0, map_size_y)
-    axes.scatter(ant_x, ant_y, color="black",s=1)# 绘制蚂蚁
+    axes.scatter(ant_x, ant_y, color="red",s=1)
+    # axes.imshow(np.flipud(ObstacleMap),cmap="binary")
+    axes.imshow(ObstacleMap,cmap="binary")
+
     #绘制信息素浓度
     map_all_x=[]
     map_all_y=[]
     max_info_density=np.max(InfoDensityMap)
+    if max_info_density==0:
+        plt.pause(0.01)
+        continue
+
     for i in range(map_size_x):
         for j in range(map_size_y):
             if InfoDensityMap[i][j]/max_info_density<0.05:continue
             map_all_x.append(i)
             map_all_y.append(j)
+
     #归一化
     color_info_density=InfoDensityMap[map_all_x, map_all_y]
     max_color=np.max(color_info_density)
@@ -184,7 +291,6 @@ while True:
 
     plt.pause(0.01)
     
-    #TODO:增加信息素更新
-
+    #TODO 多线程加速
     
 
---
+++ map_creator.py
@@ -0,0 +1,127 @@
+import pygame
+import numpy as np
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+import math
+
+WIDTH, HEIGHT = 300, 300
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+class Terrain:
+    def __init__(self, accuracy, brush_size):
+        self.grid = np.zeros((WIDTH // accuracy, HEIGHT // accuracy))
+        self.is_drawing = False
+        self.start_pos = None
+        self.accuracy = accuracy
+        self.brush_size = brush_size
+
+    def draw(self, pos,color=1):
+        x, y = pos[0] // self.accuracy, pos[1] // self.accuracy
+        if self.is_drawing:
+            if self.start_pos is None:
+                self.start_pos = x, y
+            else:
+                for i in range(-self.brush_size // 2, self.brush_size // 2 + 1):
+                    for j in range(-self.brush_size // 2, self.brush_size // 2 + 1):
+                        if math.sqrt(i**2+j**2)>=(self.brush_size/2):
+                            #圆画笔
+                            continue
+                        if (i + x) >= 0 and (i + x) < WIDTH//self.accuracy and (j + y) >= 0 and (j + y) < HEIGHT//self.accuracy:
+                            self.grid[i + x][j + y] = color
+                self.start_pos = x, y
+
+    def reset(self):
+        self.grid = np.zeros((WIDTH // self.accuracy, HEIGHT // self.accuracy))
+
+    def get_terrain(self):
+        return self.grid
+
+# def to_bitmap(terrain):
+#     """
+#     将最终绘制得到的障碍区域数据缩放到300 x 300 个像素，然后转换为一张对应的bitmap图像
+#     terrain: Terrain对象
+#     return: PIL Image对象
+#     """
+#     scale = 10
+#     obstacle_data = np.repeat(np.repeat(terrain.get_terrain(), scale, axis=0), scale, axis=1)
+#     obstacle_data = np.concatenate([obstacle_data, np.zeros((scale, obstacle_data.shape[1]))], axis=0)
+#     obstacle_data = np.concatenate([obstacle_data, np.zeros((obstacle_data.shape[0], scale))], axis=1)
+#     obstacle_data[obstacle_data > 0] = 255
+#     obstacle_img = Image.fromarray(np.uint8(obstacle_data), 'L')
+#     obstacle_img = obstacle_img.resize((WIDTH, HEIGHT), Image.NEAREST)
+#     return obstacle_img
+
+#画笔大小
+brush_size=10
+accuracy=1
+terrain = Terrain(accuracy, brush_size)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("地形绘制程序")
+
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            terrain.is_drawing = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            terrain.is_drawing = False
+            terrain.start_pos = None
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                terrain.reset()
+            elif event.key == pygame.K_LEFT:
+                terrain.brush_size = max(1, terrain.brush_size - 1)
+                print(terrain.brush_size)
+            elif event.key == pygame.K_RIGHT:
+                # terrain.brush_size = min(10, terrain.brush_size + 1)
+                terrain.brush_size = terrain.brush_size + 1
+                print(terrain.brush_size)
+            # elif event.key == pygame.K_UP:
+            #     terrain.accuracy = min(20, terrain.accuracy + 2)
+            #     terrain.grid = np.zeros((WIDTH // terrain.accuracy, HEIGHT // terrain.accuracy))
+            # elif event.key == pygame.K_DOWN:
+            #     terrain.accuracy = max(2, terrain.accuracy - 2)
+            #     terrain.grid = np.zeros((WIDTH // terrain.accuracy, HEIGHT // terrain.accuracy))
+
+
+        if pygame.mouse.get_pressed()[0]:
+            #左键画图
+            terrain.draw(pygame.mouse.get_pos(),1)
+        if pygame.mouse.get_pressed()[2]:
+            #右键擦除
+            terrain.draw(pygame.mouse.get_pos(),0)
+        
+    screen.fill(WHITE)
+
+    for i in range(WIDTH // terrain.accuracy):
+        for j in range(HEIGHT // terrain.accuracy):
+            if terrain.grid[i][j]:
+                pygame.draw.rect(screen, BLACK, (i*terrain.accuracy, j*terrain.accuracy, terrain.accuracy, terrain.accuracy))
+
+    pygame.display.flip()
+
+pygame.quit()
+
+# terrainImage=to_bitmap(terrain)
+# plt.imshow(terrainImage)#plt image
+# image_array=np.transpose(np.array(terrainImage))
+# cv2.imshow("123",image_array)
+# cv2.waitKey(0)
+# cv2.imwrite('map1.png',image_array)
+
+terrainImage=np.transpose(terrain.get_terrain())
+terrainImage=1-terrainImage
+terrainImage=terrainImage*255
+# cv2.imshow("123",terrainImage)
+# cv2.waitKey(0)
+cv2.imwrite('map1.png',terrainImage)
